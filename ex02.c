e3b11a263957ebf9e156c38b5547cf305f95ac3c
Felipe Diniz <FelipeDinizdosSantos0@gmail.com>
2026-08-13 23:03:12 +0000

    feat: implementa exercicio 2

--- ex02.c
+++ ex02.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 int main() {
+    printf("Mackenzie  - FCI");
     
+    printf("\n");
+
+    printf("Algoritimos e Programação II");
+    
+    printf("\n");
+    printf("\n");
+
+    printf(">>> ANO DE FUNDAÇAO DA FCI <<<");
+    printf("\n");
+    printf("1970");
+    printf("\n");
+
     return 0;
 }
